5597746defcc2d87af064fd5760d409aff445570
akothouma <lornaakothpb@gmail.com>
2024-11-15 23:57:53 +0300

    Refactor:addind all fields of data.json

--- src/Data.js
+++ src/Data.js
@@ -26,9 +26,10 @@ export const data=[
 },
 {
 "id":4,
-"title":" ASCII-ART",
-"languages":["Golang"],
+"title":" PORTFOLIO",
+"languages":["Javascript(React)"],
 "synopsis":"This project is about cosuming stored data through API's and displaying it on a webPage",
+"url":"https://github.com/akothouma/portfoilio2.git",
 "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages."
 }
 
